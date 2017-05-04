02e699699758e23166ee143a7fcad57e5b0f5ad3
Richard Rodriguez <richardrodz@gmail.com>
2017-05-04 18:31:49 -0500

    Added outlets to PokemonDetailsViewContoller & Main.Storyboard

--- pokedex3/PokemonDetailsViewController.swift
+++ pokedex3/PokemonDetailsViewController.swift
@@ -11,11 +11,28 @@ import UIKit
 class PokemonDetailsViewController: UIViewController {
 
     var pokemon: Pokemon!
+    
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var mainImage: UIImageView!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+    @IBOutlet weak var defenseLabel: UILabel!
+    @IBOutlet weak var pokemonIDLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var baseAttackLabel: UILabel!
+    
+    @IBOutlet weak var evoLabel: UILabel!
+    @IBOutlet weak var currentEvoImage: UIImageView!
+    @IBOutlet weak var nextEvoImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        nameLabel.text = pokemon.name
     }
 
+    @IBAction func backButtonPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
 }
